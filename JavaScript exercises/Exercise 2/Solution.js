1e1ab5afc835b76c1d5711a51b9e60ac84aa671f
Marwin Nortington <30412786+MarwinNortington@users.noreply.github.com>
2019-07-29 10:40:54 +0700

    Add files via upload

---
+++ JavaScript exercises/Exercise 2/Solution.js
@@ -0,0 +1,21 @@
+
+stringArray = ['hey','yo','mike','yo,'hey','yo','what'];
+
+let frequency = {};
+let max = 0, count = 0;
+let result;
+let word_from_store;
+
+for(let word in stringArray) {
+  word_from_store = stringArray[word];
+  frequency[word_from_store] = (frequency[word_from_store] || 0) + 1;
+  
+  if(frequency[word_from_store] > max){
+    count++;
+    max = frequency[word_from_store];
+    result = word_from_store;
+  }
+  
+}
+console.log(`The word that appears most: ${result}`);
+console.log(`Number of times: ${count}`);
